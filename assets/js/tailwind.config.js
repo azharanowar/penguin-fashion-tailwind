b7cac1702a99e9f5d2f1b1d278f6c9c4bb142f8b
azharanowar <azharanowar@gmail.com>
2022-07-31 15:16:29 +0600

    Hero section completed with tailwind css.

--- assets/js/tailwind.config.js
+++ assets/js/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */ 
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'hero-bg': '#FFFBF0',
+        'primary-color': '#FABE4C',
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
