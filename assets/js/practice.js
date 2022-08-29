b03fbd2a54f53bdf281d3d1b81338df79c5cfcac
azharanowar <azharanowar@gmail.com>
2022-08-29 22:51:00 +0600

    JS Dom practice

---
+++ assets/js/practice.js
@@ -0,0 +1,6 @@
+const womenJacket = document.getElementById("womenJacket").innerText;
+console.log(womenJacket);
+
+document.getElementById("headerBuyNowBtn").addEventListener('click', function() {
+    document.getElementById("headerTitle").innerText = "I AM PENGUIN";
+});
